00a7b0d41091c0ef9992e3a767edb04c16cba439
SandrineRV <sandrine.veloso@gmail.com>
2023-03-09 17:52:07 +0100

    test correction

--- tests/test_fast_api.py
+++ tests/test_fast_api.py
@@ -7,11 +7,27 @@ from fast_api import app
 
 client = TestClient(app)
 
+# Test communication with API
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"health_check":"OK"}
 
+# test retour des code erreurs
+def test_error():
+    response = client.post("/predict_don'texist",  follow_redirects=True)
+    assert response.status_code == 404
+# Test of API prediction format
 def test_prediction():
-    response = client.post("/predict", json={"SKID":"100001"})
+    response = client.post("/predict", json={"SKID":str(100001)})
     assert response.status_code == 200
+    # In function of models ! the output can change so ! == here
+
+
+      def test_settings_passed(self):
+        response = self.client.get('/test', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_settings_failed(self):
+        response = self.client.get('/test_not_exist', follow_redirects=True)
+        self.assertEqual(response.status_code, 404)
