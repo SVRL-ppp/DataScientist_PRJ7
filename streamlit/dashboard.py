2b50ec791a6d9fce7ec9d5cde076b051b3640e4c
SandrineRV <sandrine.veloso@gmail.com>
2023-03-11 11:14:29 +0100

    dashboard update

--- streamlit/dashboard.py
+++ streamlit/dashboard.py
@@ -36,6 +36,12 @@ with open(api_path + 'BestModel_FeatureImportance.pkl', 'rb') as f:
 with open(api_path+'bestmodel_threshold.pkl', 'rb') as f:
     model_threshold = pickle.load(f)
 
+# for LIME explainer : 
+# Load of best model explainer
+with open(api_path + "LIME_explainer", 'rb') as in_strm:
+    explainer = dill.load(in_strm)
+
+
 # --------------------------------------------------------------------------------------------------------------
 # FUNCTIONS
 # --------------------------------------------------------------------------------------------------------------
@@ -117,56 +123,6 @@ def main():
                     st.markdown("**Type Suite**: " + df_test["NAME TYPE SUITE"].loc[df_test["SK ID CURR"] == client_choice].iloc[0], unsafe_allow_html=True)
                     st.write("---" * 40)
 
-                    st.header("*Related display*")
-                    st.subheader("*Visualization*")
-                    # ----------------------------------------------------------
-                    # Graphic 1 : features distribution 
-                    # ----------------------------------------------------------
-                    st.markdown('This first visual display allow to observe the distribution of all client parameters according to specific categories.<br>', unsafe_allow_html=True)
-                    categorical_columns = [col for col in df_test.columns if df_test[col].dtype == 'object']
-                    feature = st.selectbox("Feature :", [col for col in df_test.columns if col not in categorical_columns])
-                    category = st.selectbox("Category :", [col for col in categorical_columns if col not in ["ORGANIZATION TYPE"]],key="CAT1") # We remove "ORGANIZATION TYPE" here 'cause the feature possess too much mode
-
-                    fig = px.histogram(df_test, x=feature, color=category, marginal="box", nbins=20, title='Distribution of the parameters : <b>'+feature.replace("_"," ").capitalize()+\
-                                    "<br><br><sup><span style='color:blue'>Categorical value for the current client: "+df_test[category].loc[df_test["SK ID CURR"] == client_choice].iloc[0]+"</sup>",\
-                        opacity=0.2, color_discrete_sequence=px.colors.qualitative.Light24, barmode='overlay', width=800, height=400)
-
-                    fig.update_layout(yaxis=dict(title_text="Count"),\
-                        xaxis=dict(title_text=feature),\
-                        titlefont=dict(size =18, color='black'),\
-                        legend=dict(orientation="h", itemwidth=40, y=-.15, x=.5, xanchor="center", bordercolor="Black", borderwidth=.7, title_text="<b>"+category+"</b> :"))
-
-                    fig.add_vline(x=float(df_test[feature].loc[df_test["SK ID CURR"] == client_choice].values), line_dash = 'dash', line_width=2, line_color = 'black')
-                    # Fake plot for the legend    
-                    fig.add_trace(go.Scatter(x=[df_test[feature].mean(), df_test[feature].mean()], y=[0,0], mode='lines', line=dict(color='black', width=2, dash='dash'), name='Current client positioning'))
-                    st.plotly_chart(fig, use_container_width=True)
-
-                    st.write("---" * 40) # Add splitting line
-                    # ----------------------------------------------------------
-                    # Graphic 2 : Biplot
-                    # ----------------------------------------------------------
-                    st.markdown('This second visual display allow to observe the distribution one numerical client parameters according to an another. \nPlus, this display can be colored by a specific categorie, allowing a better understanding of the client profil.<br>', unsafe_allow_html=True)
-                    feature1 = st.selectbox("Feature 1 :", [col for col in df_test.columns if col not in categorical_columns])
-                    feature2 = st.selectbox("Feature 2 :", [col for col in df_test.columns if col not in categorical_columns])
-                    category2 = st.selectbox("Category :", [col for col in categorical_columns if col not in ["ORGANIZATION TYPE"]], key="CAT2")
-
-                    # Plot :
-                    fig = px.scatter(df_test, x=feature1, y=feature2, color=category2, width=800, height=600, opacity=0.4,
-                        trendline="ols", trendline_scope="overall",trendline_color_override="black",
-                        color_discrete_sequence=px.colors.qualitative.Light24, title='<b>'+feature2+"</b> in function of <b>"+feature1+'</b> according to the <b>'+category2+
-                            "<br><br><sup><span style='color:blue'>Categorical value for the current client: "+df_test[category2].loc[df_test["SK ID CURR"] == client_choice].iloc[0]+"</sup>")
-                    fig.update_traces(marker={'size': 5})
-                    fig.update_layout(yaxis=dict(title_text=feature2),
-                        xaxis=dict(title_text=feature1),
-                        titlefont=dict(size =18, color='black'),
-                        legend=dict(orientation="h", itemwidth=40, y=-.15, x=.5,xanchor="center", bordercolor="Black", borderwidth=.7, title_text="<b>"+category+"</b> :"))
-                    fig.add_trace(go.Scattergl(x=[float(df_test[feature1].loc[df_test["SK ID CURR"] == client_choice].values)],\
-                        y=[float(df_test[feature2].loc[df_test["SK ID CURR"] == client_choice].values)],mode="markers",
-                                    marker=dict(color="black", size=10, symbol="star"), name="Current client positioning"))
-                    st.plotly_chart(fig, use_container_width=True)
-                        
-                    st.write("---" * 40)  # Add splitting line
-
             with credit_tab:
                 # ----------------------------------------------------------
                 # New subsection :
@@ -224,7 +180,56 @@ def main():
             # ----------------------------------------------------------------------------------------
             # Fird tab : Plots
             # ----------------------------------------------------------------------------------------
-            # with entities_tab:
+            with entities_tab:
+                st.header("*Related display*")
+                st.subheader("*Visualization*")
+                # ----------------------------------------------------------
+                # Graphic 1 : features distribution 
+                # ----------------------------------------------------------
+                st.markdown('This first visual display allow to observe the distribution of all client parameters according to specific categories.<br>', unsafe_allow_html=True)
+                categorical_columns = [col for col in df_test.columns if df_test[col].dtype == 'object']
+                feature = st.selectbox("Feature :", [col for col in df_test.columns if col not in categorical_columns])
+                category = st.selectbox("Category :", [col for col in categorical_columns if col not in ["ORGANIZATION TYPE"]],key="CAT1") # We remove "ORGANIZATION TYPE" here 'cause the feature possess too much mode
+
+                fig = px.histogram(df_test, x=feature, color=category, marginal="box", nbins=20, title='Distribution of the parameters : <b>'+feature.replace("_"," ").capitalize()+\
+                                "<br><br><sup><span style='color:blue'>Categorical value for the current client: "+df_test[category].loc[df_test["SK ID CURR"] == client_choice].iloc[0]+"</sup>",\
+                    opacity=0.2, color_discrete_sequence=px.colors.qualitative.Light24, barmode='overlay', width=800, height=400)
+
+                fig.update_layout(yaxis=dict(title_text="Count"),\
+                    xaxis=dict(title_text=feature),\
+                    titlefont=dict(size =18, color='black'),\
+                    legend=dict(orientation="h", itemwidth=40, y=-.15, x=.5, xanchor="center", bordercolor="Black", borderwidth=.7, title_text="<b>"+category+"</b> :"))
+
+                fig.add_vline(x=float(df_test[feature].loc[df_test["SK ID CURR"] == client_choice].values), line_dash = 'dash', line_width=2, line_color = 'black')
+                # Fake plot for the legend    
+                fig.add_trace(go.Scatter(x=[df_test[feature].mean(), df_test[feature].mean()], y=[0,0], mode='lines', line=dict(color='black', width=2, dash='dash'), name='Current client positioning'))
+                st.plotly_chart(fig, use_container_width=True)
+
+                st.write("---" * 40) # Add splitting line
+                # ----------------------------------------------------------
+                # Graphic 2 : Biplot
+                # ----------------------------------------------------------
+                st.markdown('This second visual display allow to observe the distribution one numerical client parameters according to an another. \nPlus, this display can be colored by a specific categorie, allowing a better understanding of the client profil.<br>', unsafe_allow_html=True)
+                feature1 = st.selectbox("Feature 1 :", [col for col in df_test.columns if col not in categorical_columns])
+                feature2 = st.selectbox("Feature 2 :", [col for col in df_test.columns if col not in categorical_columns])
+                category2 = st.selectbox("Category :", [col for col in categorical_columns if col not in ["ORGANIZATION TYPE"]], key="CAT2")
+
+                # Plot :
+                fig = px.scatter(df_test, x=feature1, y=feature2, color=category2, width=800, height=600, opacity=0.4,
+                    trendline="ols", trendline_scope="overall",trendline_color_override="black",
+                    color_discrete_sequence=px.colors.qualitative.Light24, title='<b>'+feature2+"</b> in function of <b>"+feature1+'</b> according to the <b>'+category2+
+                        "<br><br><sup><span style='color:blue'>Categorical value for the current client: "+df_test[category2].loc[df_test["SK ID CURR"] == client_choice].iloc[0]+"</sup>")
+                fig.update_traces(marker={'size': 5})
+                fig.update_layout(yaxis=dict(title_text=feature2),
+                    xaxis=dict(title_text=feature1),
+                    titlefont=dict(size =18, color='black'),
+                    legend=dict(orientation="h", itemwidth=40, y=-.15, x=.5,xanchor="center", bordercolor="Black", borderwidth=.7, title_text="<b>"+category+"</b> :"))
+                fig.add_trace(go.Scattergl(x=[float(df_test[feature1].loc[df_test["SK ID CURR"] == client_choice].values)],\
+                    y=[float(df_test[feature2].loc[df_test["SK ID CURR"] == client_choice].values)],mode="markers",
+                                marker=dict(color="black", size=10, symbol="star"), name="Current client positioning"))
+                st.plotly_chart(fig, use_container_width=True)
+                        
+                st.write("---" * 40)  # Add splitting line
             
                     
             with feat_impt:
@@ -242,7 +247,7 @@ def main():
                 best_features = feature_importance[["feature", "importance"]].sort_values(by="importance", ascending=False)[:number]
                 set_color = px.colors.sample_colorscale("turbo", [n/(number -1) for n in range(number)])
 
-                fig = px.bar(best_features, y='feature', x='importance', height=600, width=800,\
+                fig = px.bar(best_features, y='feature', x='importance', height=500, width=600,\
                     hover_name="feature", title= "<b>Global features importances</b> : Top " + str(number) + " features",\
                     color='feature', color_discrete_sequence=set_color)
                 fig.update_layout(yaxis=dict(title_text="<b>Feature"),\
@@ -263,7 +268,11 @@ def main():
                 st.markdown("In this section, the **major feature** involved in credit acceptance or rejection are display **for the selected client**.<br> \
                         You will be able to see wich feature and the exact value mainly responsable for the credit status.<br>\
                         In blue are indicated feature influencing in credit acceptance and in orange the rejection.",unsafe_allow_html=True)
-
+                id_numb = df_test[feats].loc[df_test['SK_ID_CURR']==client_choice].index[0]
+                explanation = explainer.explain_instance(np.array(df_test[feats])[id_numb], model.predict_proba, num_features=10)
+                html_lime=explanation.as_html()
+                components.v1.html(html_lime, width=1000, height=350, scrolling=True)
+                st.write("---" * 40) # Add splitting line
 
 if __name__ == '__main__':
     main()
